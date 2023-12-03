083f8ad1cd9ad428e473383b1afa64a2baee4924
ZhangAmyyy <zhangyy_amy@outlook.com>
2023-12-03 11:41:42 -0800

    use mongo atlas

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ import mongoose from "mongoose";
 // mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 // const uri = "mongodb+srv://test:<password>@atlascluster.wedmycv.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect("mongodb+srv://test:123@atlascluster.wedmycv.mongodb.net/?retryWrites=true&w=majority");
+// mongoose.connect("mongodb+srv://test:123@atlascluster.wedmycv.mongodb.net/?retryWrites=true&w=majority");
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(
     cors({
